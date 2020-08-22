895497c8056dd6195e8f7e6bc6405b3728df3a0e
dp3 <dp3athome@gmail.com>
2020-08-22 18:50:16 -0500

    update to seamonkey

--- seamonkey/tools/chocolateyinstall.ps1
+++ seamonkey/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'seamonkey'
 $fileType = 'exe'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /S /quiet'
-$url = 'https://archive.mozilla.org/pub/seamonkey/releases/2.49.5/win64/en-US/seamonkey-2.49.5.installer.exe'
-$checksum = 'e0cdf6e1fc8cd470aa19e8d791347cf7277fdcca5b8f42424110807a95e086d5'
+$url = 'https://archive.mozilla.org/pub/seamonkey/releases/2.53.3/win32/en-US/seamonkey-2.53.3.en-US.win32.installer.exe'
+$checksum = 'f5a589a5d2349d0f93f4842fc8014a6883ebd78019382ba3f157883f2d478b7fs'
 $checksumType = 'sha256'
 Install-ChocolateyPackage "$packageName" "$fileType" "$silentArgs" "$url" "$url64" "$checksum" "$checksumType"
