b4c6c83ee61b41433b0d516d12fb6d482aa16a71
dp3 <dp3athome@gmail.com>
2020-08-22 18:49:49 -0500

    xournal++ edit typos

--- xournal++/tools/chocolateyinstall.ps1
+++ xournal++/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
-  packageName  = 'xournal++'
+  packageName  = 'xournalpp'
   url          = 'https://github.com/xournalpp/xournalpp/releases/download/1.0.18/xournalpp-1.0.18-windows.zip'
   checksum     = 'af14a523fb160a0d1c3fecc9025060ba897b2fab93ddcff5c9bfcbb48951678b'
   checksumType = 'sha256'
