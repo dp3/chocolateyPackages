ddba5ee52c0a00d692243fa5d912aa2d7f28078d
Devon <dp3athome@yahoo.com>
2020-08-22 17:17:02 -0500

    adding xournal++

---
+++ xournal++/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName  = 'xournal++'
+  url          = 'https://github.com/xournalpp/xournalpp/releases/download/1.0.18/xournalpp-1.0.18-windows.zip'
+  checksum     = 'af14a523fb160a0d1c3fecc9025060ba897b2fab93ddcff5c9bfcbb48951678b'
+  checksumType = 'sha256'
+  unzipLocation = "$(split-path -parent $MyInvocation.MyCommand.Definition)"
+}
+
+Install-ChocolateyZipPackage @packageArgs
