61c238219750959f9121561cc3a6981bcd881ff8
dp3 <dp3athome@gmail.com>
2020-08-22 23:17:36 -0500

    fixed the broken install issue, added icon url

--- xournal++/tools/chocolateyinstall.ps1
+++ xournal++/tools/chocolateyinstall.ps1
@@ -1,11 +1,24 @@
 ﻿$ErrorActionPreference = 'Stop'
 
+$packageName= 'xournalpp'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+#$fileLocation = Join-Path $toolsDir 'xournalpp-1.0.18-windows.zip'
+
+
 $packageArgs = @{
-  packageName  = 'xournalpp'
+  packageName  = $packageName
+  unzipLocation = $toolsDir
   url          = 'https://github.com/xournalpp/xournalpp/releases/download/1.0.18/xournalpp-1.0.18-windows.zip'
   checksum     = 'af14a523fb160a0d1c3fecc9025060ba897b2fab93ddcff5c9bfcbb48951678b'
   checksumType = 'sha256'
-  unzipLocation = "$(split-path -parent $MyInvocation.MyCommand.Definition)"
 }
 
 Install-ChocolateyZipPackage @packageArgs
+
+$ErrorActionPreference = 'Stop'
+
+$fileType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /S /quiet'
+$file = 'C:\ProgramData\chocolatey\lib\xournalpp\tools\xournalpp-1.0.18-windows.exe'         
+  
+Install-ChocolateyPackage "$packageName" "$fileType" "$silentArgs" "$file" "$checksum" "$checksumType"
