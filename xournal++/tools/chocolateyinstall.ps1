0fe7820a7d79f561a357dcef51ae5f222ddda15b
Devon Peel <dp3athome@gmail.com>
2020-12-10 21:47:58 -0800

    updating xournal

--- xournal++/tools/chocolateyinstall.ps1
+++ xournal++/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName  = $packageName
   unzipLocation = $toolsDir
-  url          = 'https://github.com/xournalpp/xournalpp/releases/download/1.0.18/xournalpp-1.0.18-windows.zip'
-  checksum     = 'af14a523fb160a0d1c3fecc9025060ba897b2fab93ddcff5c9bfcbb48951678b'
+  url          = 'https://github.com/xournalpp/xournalpp/releases/download/1.0.19/xournalpp-1.0.19-windows.zip'
+  checksum     = '5c7dca35cf3fbbeebf93b55e4239e05822342457ab06716b14bca8bb0f48c3a9'
   checksumType = 'sha256'
 }
 
