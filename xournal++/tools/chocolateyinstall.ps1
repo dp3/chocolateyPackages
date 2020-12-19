32de026554906d504cf42acc647201a28698c4d9
Devon Peel <dp3athome@gmail.com>
2020-12-19 11:16:25 -0800

    adding xournal++ 1.20

--- xournal++/tools/chocolateyinstall.ps1
+++ xournal++/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName  = $packageName
   unzipLocation = $toolsDir
-  url          = 'https://github.com/xournalpp/xournalpp/releases/download/1.0.19/xournalpp-1.0.19-windows.zip'
-  checksum     = '5c7dca35cf3fbbeebf93b55e4239e05822342457ab06716b14bca8bb0f48c3a9'
+  url          = 'https://github.com/xournalpp/xournalpp/releases/download/1.0.20/xournalpp-1.0.20-windows.zip'
+  checksum     = 'c75690c02804cd00f8e11832600e7468a525b793cbccf6f7d68cd80e923eb02e'
   checksumType = 'sha256'
 }
 
@@ -18,6 +18,6 @@ $ErrorActionPreference = 'Stop'
 $fileType = 'exe'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /S /quiet'
 
-$file = $toolsDir+'\xournalpp-1.0.18-windows.exe'
+$file = $toolsDir+'\xournalpp-1.0.20-windows.exe'
  
 Install-ChocolateyPackage "$packageName" "$fileType" "$silentArgs" "$file" "$checksum" "$checksumType"
