4f7ca7339b8bc872e990b47eec1f9400e7f17107
Devon Peel <dp3athome@gmail.com>
2023-04-05 22:27:06 -0500

    adding seamonkey 2.53.16

--- seamonkey/tools/chocolateyinstall.ps1
+++ seamonkey/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'seamonkey'
 $fileType = 'exe'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /S /quiet'
-$url = 'https://archive.mozilla.org/pub/seamonkey/releases/2.53.13/win32/en-US/seamonkey-2.53.13.en-US.win32.installer.exe'
-$checksum ='af77de7c66fc6878cba77738bdfe03f80f4c753a428f373b9a1bae13c5fa125d`'
+$url = 'https://archive.mozilla.org/pub/seamonkey/releases/2.53.16/win32/en-US/seamonkey-2.53.16.en-US.win32.installer.exe'
+$checksum ='74b59d6b678035820cb7ee15d7ea549130111a7290b7e83e09d1d20db3a6edb7'
 $checksumType = 'sha256'
 Install-ChocolateyPackage "$packageName" "$fileType" "$silentArgs" "$url" "$url64" "$checksum" "$checksumType"
