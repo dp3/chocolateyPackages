6134f94a862c3b979f49b144a0fde08cbd116981
Devon Peel <dp3athome@gmail.com>
2025-02-02 16:27:34 -0600

    updating seamonkey to 2.53.20

--- seamonkey/tools/chocolateyinstall.ps1
+++ seamonkey/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'seamonkey'
 $fileType = 'exe'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /S /quiet'
-$url = 'https://archive.mozilla.org/pub/seamonkey/releases/2.53.18/win32/en-US/seamonkey-2.53.18.en-US.win32.installer.exe'
-$checksum ='3b6d8be0fd7d39b019cc9f5c8875595d22e70ef372a4900e73e2b5399f6da603'
+$url = 'https://archive.seamonkey-project.org/releases/2.53.20/win32/en-US/seamonkey-2.53.20.en-US.win32.installer.exe'
+$checksum ='e36a96e3598c3637b770443c3f3350a524ce603c69a4e40e5c514b9113d2720e'
 $checksumType = 'sha256'
 #Get-ChecksumValid -File $fileFullPath -CheckSum $checksum -ChecksumType $checksumType
 Install-ChocolateyPackage "$packageName" -FileType "$fileType" -SilentArgs "$silentArgs" -Url "$url" -Checksum "$checksum" -ChecksumType "$checksumType"
